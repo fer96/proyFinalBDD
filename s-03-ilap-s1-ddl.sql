494a263648985087d21095867d84d1b54d6b720e
fer96 <fer96.dlrs@gmail.com>
2018-12-01 23:05:22 -0600

    Add scripts DDL sitio 1 y 3

---
+++ s-03-ilap-s1-ddl.sql
@@ -0,0 +1,5 @@
+--@Author: De La Rosa Salas Fernando
+--					Mejia Valdiviezo Ernesto Daniel
+--@Fecha creación: 01/12/2018
+--@Descripcion: Codigo DDL para el sitio 1
+
---
+++ s-03-ilap-s3-ddl.sql
@@ -0,0 +1,4 @@
+--@Author: De La Rosa Salas Fernando
+--					Mejia Valdiviezo Ernesto Daniel
+--@Fecha creación: 01/12/2018
+--@Descripcion: Codigo DDL para el sitio 3
