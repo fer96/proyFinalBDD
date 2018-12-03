a62adad5196ab39cecf0db6ccd4ae660f22d69ff
fer96 <fer96.dlrs@gmail.com>
2018-12-02 21:08:02 -0600

    Add scripts valida y main para sinonimos

---
+++ s-04-ilap-main-sinonimos.sql
@@ -0,0 +1,38 @@
+--@Author: De La Rosa Salas Fernando
+--					Mejia Valdiviezo Ernesto Daniel
+--@Fecha creación: 02/12/2018
+--@Descripcion: Script main sinonimos
+
+clear screen
+whenever sqlerror exit rollback;
+
+prompt ===================================== 
+prompt Creando sinonimos para fdlrsbd_s1 
+prompt ===================================== 
+connect ilap_bdd/ilap_bdd@fdlrsbd_s1 
+@s-04-ilap-fdlrsbd-s1-sinonimos.sql 
+@s-04-ilap-valida-sinonimos.sql
+
+prompt ===================================== 
+prompt creando sinonimos para mved_s1 
+prompt ===================================== 
+connect ilap_bdd/ilap_bdd@mved_s1 
+@s-04-ilap-mved-s2-sinonimos.sql 
+@s-04-ilap-valida-sinonimos.sql
+
+prompt =====================================
+prompt creando sinonimos para fldrsbd_s2 
+prompt ===================================== 
+connect ilap_bdd/ilap_bdd@fldrsbd_s2 
+@s-04-ilap-fldrsbd-s2-sinonimos.sql 
+@s-04-ilap-valida-sinonimos.sql
+
+prompt ===================================== 
+prompt creando sinonimos para mved_s2 
+prompt ===================================== 
+connect ilap_bdd/ilap_bdd@mved_s2 
+@s-04-ilap-mved-s4-sinonimos.sql 
+@s-04-ilap-valida-sinonimos.sql
+
+prompt Listo!
+exit;
---
+++ s-04-ilap-valida-sinonimos.sql
@@ -0,0 +1,106 @@
+--@Author: De La Rosa Salas Fernando
+--					Mejia Valdiviezo Ernesto Daniel
+--@Fecha creación: 02/12/2018
+--@Descripcion: Script valida sinonimos
+
+Prompt validando sinonimos para sucursal 
+select count(*) from sucursal_f1
+union
+select count(*) from sucursal_f2
+union
+select count(*) from sucursal_f3 
+union
+select count(*) from sucursal_f4;
+
+Prompt validando sinonimos para sucursal_taller 
+select count(*) from sucursal_taller_f1
+union
+select count(*) from sucursal_taller_f2
+union
+select count(*) from sucursal_taller_f3 
+union
+select count(*) from sucursal_taller_f4;
+
+Prompt validando sinonimos para sucursal_venta 
+select count(*) from sucursal_venta_f1
+union
+select count(*) from sucursal_venta_f2
+union
+select count(*) from sucursal_venta_f3 
+union
+select count(*) from sucursal_venta_f4;
+
+Prompt validando sinonimos para laptop 
+select count(*) from laptop_f1
+union
+select count(*) from laptop_f2
+union
+select count(*) from laptop_f3 
+union
+select count(*) from laptop_f4
+union
+select count(*) from laptop_f5;
+
+Prompt validando sinonimos para laptop_inventario 
+select count(*) from laptop_inventario_f1
+union
+select count(*) from laptop_inventario_f2;
+
+Prompt validando sinonimos para historio_status_laptop 
+select count(*) from historio_status_laptop_f1
+union
+select count(*) from historio_status_laptop_f2;
+
+Prompt validando sinonimos para servicio_laptop 
+select count(*) from servicio_laptop_f1
+union
+select count(*) from servicio_laptop_f2
+union
+select count(*) from servicio_laptop_f3 
+union
+select count(*) from servicio_laptop_f4;
+
+Prompt validando sinonimos para tipo_procesador 
+select count(*) from tipo_procesador_s1
+union
+select count(*) from tipo_procesador_s2
+union
+select count(*) from tipo_procesador_s3 
+union
+select count(*) from tipo_procesador_s4;
+
+Prompt validando sinonimos para tipo_tarjeta_video 
+select count(*) from tipo_tarjeta_video_s1
+union
+select count(*) from tipo_tarjeta_video_s2
+union
+select count(*) from tipo_tarjeta_video_s3 
+union
+select count(*) from tipo_tarjeta_video_s4;
+
+Prompt validando sinonimos para tipo_almacenamiento 
+select count(*) from tipo_almacenamiento_s1
+union
+select count(*) from tipo_almacenamiento_s2
+union
+select count(*) from tipo_almacenamiento_s3 
+union
+select count(*) from tipo_almacenamiento_s4;
+
+Prompt validando sinonimos para tipo_monitor 
+select count(*) from tipo_monitor_s1
+union
+select count(*) from tipo_monitor_s2
+union
+select count(*) from tipo_monitor_s3 
+union
+select count(*) from tipo_monitor_s4;
+
+Prompt validando sinonimos para status_laptop 
+select count(*) from status_laptop_s1
+union
+select count(*) from status_laptop_s2
+union
+select count(*) from status_laptop_s3 
+union
+select count(*) from status_laptop_s4;
