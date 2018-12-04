b72349a505d865d77d724f74cec9980a74c231e1
Ernesto Daniel Mejia Valdiviezo <netosaurio11@MacBook-Air-de-Ernesto.local>
2018-12-03 21:13:35 -0600

    Se añaden triggers para sitio 2 y 4 de ciertas tablas

---
+++ s-06-ilap-mved-s2-sucursal-venta-trigger.sql
@@ -0,0 +1,90 @@
+--@Author: De La Rosa Salas Fernando
+--					Mejia Valdiviezo Ernesto Daniel
+--@Fecha creación: 03/12/2018
+--@Descripcion: Creacion de trigger para la tabla sucursal_venta sitio 2
+
+create or replace trigger t_dml_sucursal_venta_s2
+instead of insert or delete on sucursal_venta
+declare
+v_count number;
+begin
+	case
+		when inserting then
+			select count(*) into v_count
+			from sucursal_venta_f2
+			where sucursal_id = :new.sucursal_id;
+			--inserción local
+			if v_count > 0 then
+				insert into sucursal_venta_f2(sucursal_id,hora_apertura,hora_cierre)
+				values (:new.sucursal_id,:new.hora_apertura,:new.hora_cierre);
+			else
+				select count(*) into v_count
+				from sucursal_venta_f1
+				where sucursal_id = :new.sucursal_id;
+				--insercion remota en s1
+				if v_count > 0 then
+					insert into sucursal_venta_f1(sucursal_id,hora_apertura,hora_cierre)
+					values (:new.sucursal_id,:new.hora_apertura,:new.hora_cierre);
+				else
+					select count(*) into v_count
+					from sucursal_venta_f3
+					where sucursal_id = :new.sucursal_id;
+					--insercion remota en s3
+					if v_count > 0 then
+						insert into sucursal_venta_f3(sucursal_id,hora_apertura,hora_cierre)
+						values (:new.sucursal_id,:new.hora_apertura,:new.hora_cierre);
+					else
+						select count(*) into v_count
+						from sucursal_venta_f4
+						where sucursal_id = :new.sucursal_id;
+						--insercion remota en s4
+						if v_count > 0 then
+							insert into sucursal_venta_f4(sucursal_id,hora_apertura,hora_cierre)
+							values (:new.sucursal_id,:new.hora_apertura,:new.hora_cierre);
+						else
+						raise_application_error(-20020,'El registro que se intenta insertar o eliminar no cumple con el esquema de fragmentación horizontal derivada.');
+						end if;
+					end if;
+				end if;
+			end if;
+
+		when updating then
+			raise_application_error(-20030,'Se intentó realizar una operación update. Para proósitos del proyecto, esta operación no estará implementada.');
+		when deleting then
+			select count(*) into v_count
+			from sucursal_venta_f2
+			where sucursal_id = :new.sucursal_id;
+			--inserción local
+			if v_count > 0 then
+				delete from sucursal_venta_f2 where sucursal_id = :old.sucursal_id
+			else
+				select count(*) into v_count
+				from sucursal_venta_f1
+				where sucursal_id = :new.sucursal_id;
+				--insercion remota en s1
+				if v_count > 0 then
+					delete from sucursal_venta_f1 where sucursal_id = :old.sucursal_id
+				else
+					select count(*) into v_count
+					from sucursal_venta_f3
+					where sucursal_id = :new.sucursal_id;
+					--insercion remota en s3
+					if v_count > 0 then
+						delete from sucursal_venta_f3 where sucursal_id = :old.sucursal_id
+					else
+						select count(*) into v_count
+						from sucursal_venta_f4
+						where sucursal_id = :new.sucursal_id;
+						--insercion remota en s4
+						if v_count > 0 then
+							delete from sucursal_venta_f4 where sucursal_id = :old.sucursal_id
+						else
+						raise_application_error(-20020,'El registro que se intenta insertar o eliminar no cumple con el esquema de fragmentación horizontal derivada.');
+						end if;
+					end if;
+				end if;
+			end if;
+	end case;
+end;
+/
+show errors;
---
+++ s-06-ilap-mved-s4-sucursal-taller-trigger.sql
@@ -0,0 +1,90 @@
+--@Author: De La Rosa Salas Fernando
+--					Mejia Valdiviezo Ernesto Daniel
+--@Fecha creación: 03/12/2018
+--@Descripcion: Creacion de trigger para la tabla sucursal_taller sitio 4
+
+create or replace trigger t_dml_sucursal_taller_s4
+instead of insert or delete on sucursal_taller
+declare
+v_count number;
+begin
+	case
+		when inserting then
+			select count(*) into v_count
+			from sucursal_taller_f4
+			where sucursal_id = :new.sucursal_id;
+			--inserción local
+			if v_count > 0 then
+				insert into sucursal_taller_f4(sucursal_id,dia_descanso,telefono_atencion)
+				values (:new.sucursal_id,:new.dia_descanso,:new.telefono_atencion);
+			else
+				select count(*) into v_count
+				from sucursal_taller_f1
+				where sucursal_id = :new.sucursal_id;
+				--insercion remota en s1
+				if v_count > 0 then
+					insert into sucursal_taller_f1(sucursal_id,dia_descanso,telefono_atencion)
+					values (:new.sucursal_id,:new.dia_descanso,:new.telefono_atencion);
+				else
+					select count(*) into v_count
+					from sucursal_taller_f3
+					where sucursal_id = :new.sucursal_id;
+					--insercion remota en s3
+					if v_count > 0 then
+						insert into sucursal_taller_f3(sucursal_id,dia_descanso,telefono_atencion)
+						values (:new.sucursal_id,:new.dia_descanso,:new.telefono_atencion);
+					else
+						select count(*) into v_count
+						from sucursal_taller_f2
+						where sucursal_id = :new.sucursal_id;
+						--insercion remota en s2
+						if v_count > 0 then
+							insert into sucursal_taller_f2(sucursal_id,dia_descanso,telefono_atencion)
+							values (:new.sucursal_id,:new.dia_descanso,:new.telefono_atencion);
+						else
+						raise_application_error(-20020,'El registro que se intenta insertar o eliminar no cumple con el esquema de fragmentación horizontal derivada.');
+						end if;
+					end if;
+				end if;
+			end if;
+
+		when updating then
+			raise_application_error(-20030,'Se intentó realizar una operación update. Para proósitos del proyecto, esta operación no estará implementada.');
+		when deleting then
+			select count(*) into v_count
+			from sucursal_taller_f4
+			where sucursal_id = :new.sucursal_id;
+			--inserción local
+			if v_count > 0 then
+				delete from sucursal_taller_f4 where sucursal_id = :old.sucursal_id
+			else
+				select count(*) into v_count
+				from sucursal_taller_f1
+				where sucursal_id = :new.sucursal_id;
+				--insercion remota en s1
+				if v_count > 0 then
+					delete from sucursal_taller_f1 where sucursal_id = :old.sucursal_id
+				else
+					select count(*) into v_count
+					from sucursal_taller_f3
+					where sucursal_id = :new.sucursal_id;
+					--insercion remota en s3
+					if v_count > 0 then
+						delete from sucursal_taller_f3 where sucursal_id = :old.sucursal_id
+					else
+						select count(*) into v_count
+						from sucursal_taller_f2
+						where sucursal_id = :new.sucursal_id;
+						--insercion remota en s4
+						if v_count > 0 then
+							delete from sucursal_taller_f2 where sucursal_id = :old.sucursal_id
+						else
+						raise_application_error(-20020,'El registro que se intenta insertar o eliminar no cumple con el esquema de fragmentación horizontal derivada.');
+						end if;
+					end if;
+				end if;
+			end if;
+	end case;
+end;
+/
+show errors;
